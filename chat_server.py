c588d229182d3452c4915513489c7843b4be937d
HandsOnExp <danielohana6@gmail.com>
2025-01-15 20:19:48 +0200

    Serve static HTML from the static directory.

--- chat_server.py
+++ chat_server.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run()
+# return home page to client
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
+
