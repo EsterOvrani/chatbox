e8f4c96a607142637358a4fc9cf6f31418d8b3c8
Chava Levi <cl6724259@gmail.com>
2025-01-15 21:14:00 +0200

    Resolve the conflict

--- chat_server.py
+++ chat_server.py
@@ -1,32 +1,18 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request
-=======
-from flask import Flask, send_file  # Import Flask and the send_file function
 
-app = Flask(__name__)  # Initialize the Flask app
->>>>>>> bfd7115fdc163b8f8eed518349587c39e8624080
+app = Flask(__name__)  # Initialize the Flask app# Chava: Define a route to serve the room page
 
-
-# Chava: Define a route to serve the room page
 @app.route('/<room>')
 def serve_room_for_room(room):
     # Return the 'index.html' file when the route is accessed
-    return send_file('index.html')
+    return render_template('index.html')
 
-<<<<<<< HEAD
 # return home page to client
 @app.route('/')
 def home_page():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
-
-
-=======
 
 # Run the app if this script is executed directly
 if __name__ == '__main__':
     app.run()  # Start the Flask development server
->>>>>>> bfd7115fdc163b8f8eed518349587c39e8624080
