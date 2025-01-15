57a1167503326e06beb52467600b24feb37c200e
HandsOnExp <danielohana6@gmail.com>
2025-01-15 21:26:57 +0200

    Implement GET /api/chat/<room> to fetch chat messages.

--- chat_server.py
+++ chat_server.py
@@ -1,32 +1,39 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request
-=======
-from flask import Flask, send_file  # Import Flask and the send_file function
+import os
 
 app = Flask(__name__)  # Initialize the Flask app
->>>>>>> bfd7115fdc163b8f8eed518349587c39e8624080
 
+# chck the folder exist - ester ovrani
+CHAT_DIR = "chats_massage"
+if not os.path.exists(CHAT_DIR):
+    os.makedirs(CHAT_DIR)
+
+# get the room chat file - ester ovrani
+def get_room_file(room):
+    return os.path.join(CHAT_DIR, f"{room}.txt")
 
 # Chava: Define a route to serve the room page
 @app.route('/<room>')
 def serve_room_for_room(room):
     # Return the 'index.html' file when the route is accessed
-    return send_file('index.html')
+    return render_template('index.html')
 
-<<<<<<< HEAD
-# return home page to client
+    return os.path.join(CHAT_DIR, f"{room}.txt")
+
+# return the room chat massage file - ester ovrani
+@app.route('/api/chat/<room>', methods=['GET'])
+def go_room_page(room):
+    try:
+        with open(get_room_file(room), 'r', encoding='utf-8') as file:
+            return file.read()
+    except FileNotFoundError:
+            return ""
+
+# return home page to client - ester ovrani
 @app.route('/')
 def home_page():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
-
-
-=======
-
 # Run the app if this script is executed directly
 if __name__ == '__main__':
     app.run()  # Start the Flask development server
->>>>>>> bfd7115fdc163b8f8eed518349587c39e8624080
